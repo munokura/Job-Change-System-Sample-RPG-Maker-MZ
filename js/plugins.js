b7c7bd73f0935eba5e514f76d2da5be354c225aa
munokura <36838778+munokura@users.noreply.github.com>
2020-10-02 14:50:48 +0900

    プラグインを追加し、スクリプトをプラグインコマンドに書き換え

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"DarkPlasma_ChoiceHelp","status":true,"description":"選択肢にヘルプを表示する","parameters":{}},
 {"name":"Mano_ChoiceCustom","status":true,"description":"選択肢で様々なことを可能にします。","parameters":{}},
-{"name":"MNKR_SelectMaxRows","status":true,"description":"選択肢ウィンドウ内の行数を指定します。","parameters":{"Max Rows":"6"}}
+{"name":"MNKR_SelectMaxRows","status":true,"description":"選択肢ウィンドウ内の行数を指定します。","parameters":{"Max Rows":"6"}},
+{"name":"MNKR_VariableCommand","status":true,"description":"変数が使用できないイベントコマンドをプラグインコマンドで可能にします。","parameters":{}}
 ];
