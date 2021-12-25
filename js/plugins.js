38b740c6b0441e6fec6f1eb4df85011320bbc191
munokura <munokura@users.noreply.github.com>
2021-12-25 17:30:38 +0900

    プラグインバージョンアップ

--- js/plugins.js
+++ js/plugins.js
@@ -5,5 +5,5 @@ var $plugins =
 {"name":"DarkPlasma_ChoiceHelp","status":true,"description":"選択肢にヘルプを表示する","parameters":{}},
 {"name":"Mano_ChoiceCustom","status":true,"description":"選択肢で様々なことを可能にします。","parameters":{}},
 {"name":"MNKR_SelectMaxRows","status":true,"description":"選択肢ウィンドウ内の行数を指定します。","parameters":{"Max Rows":"6"}},
-{"name":"MNKR_VariableCommand","status":true,"description":"変数が使用できないイベントコマンドをプラグインコマンドで可能にします。","parameters":{}}
+{"name":"MNKR_VariableCommand","status":true,"description":"イベントコマンドで変数が選べない組み合わせを使用可能にするプラグインコマンドを追加します。","parameters":{}}
 ];
